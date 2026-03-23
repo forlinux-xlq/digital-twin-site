9941d5e959942196e58c466c672d22dca39a6e77
miaoda <miaoda@baidu.com>
2026-03-23 16:37:20 +0800

    fix: 修复main.tsx导入路径

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { AppWrapper } from "./components/common/PageMeta.tsx";
+import { AppWrapper } from "./components/common/PageMeta";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
